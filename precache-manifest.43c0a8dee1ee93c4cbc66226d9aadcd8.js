cc087a65137101690f08d900cd07e3ec8f678062
bm298 <musabilal93@gmail.com>
2022-11-30 02:06:14 +0000

    Updates

---
+++ precache-manifest.43c0a8dee1ee93c4cbc66226d9aadcd8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e273eff728481a9152b6",
+    "url": "/FormUpdate/static/css/main.d7d54a9b.chunk.css"
+  },
+  {
+    "revision": "e273eff728481a9152b6",
+    "url": "/FormUpdate/static/js/main.e273eff7.chunk.js"
+  },
+  {
+    "revision": "e45e0075b0567c8b67a9",
+    "url": "/FormUpdate/static/js/1.e45e0075.chunk.js"
+  },
+  {
+    "revision": "857bb499bd5345504434",
+    "url": "/FormUpdate/static/js/2.857bb499.chunk.js"
+  },
+  {
+    "revision": "9b459d41d239075266bf",
+    "url": "/FormUpdate/static/js/runtime~main.9b459d41.js"
+  },
+  {
+    "revision": "3d16735a6abb794a24c875931034fb1a",
+    "url": "/FormUpdate/static/media/face2form.3d16735a.jfif"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/FormUpdate/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "a0c2209ed3dfce993cb3de73ee1d3992",
+    "url": "/FormUpdate/index.html"
+  }
+];
