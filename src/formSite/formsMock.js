28ef0ecf74970e1c2e74204b81dfd2da423dde23
bm298 <musabilal93@gmail.com>
2023-04-07 01:21:08 +0100

    updated form proj

--- src/formSite/formsMock.js
+++ src/formSite/formsMock.js
@@ -1,7 +1,5 @@
 import React from "react";
 import './formSiteCss.css';
-import face2form from '../formSite/face2form.jfif'
-import { FaGlobeAmericas } from 'react-icons/fa';
 import {BsCheck} from 'react-icons/bs';
 import ModalForm from '../formSite/modalForm';
 
@@ -14,6 +12,9 @@ function Nav(){
 }
 
 export function Content(){
+
+    const[openModaL, setOpenModal]= React.useState(false)
+
     const [formData, setFormData] = React.useState({
         firstName: "",
         lastName:"",
@@ -24,7 +25,6 @@ export function Content(){
     })
 
     function handleChange(event) {
-        console.log (event)
         const {name, value, type, checked} = event.target
         setFormData(prevFormData => ({
             ...prevFormData,
@@ -32,61 +32,58 @@ export function Content(){
         }))
     }
 
-    const[openModaL, setOpenModal]= React.useState(false)
-
     function handleSubmit(event) {            
 
             event.preventDefault()
-        if (formData.firstName.length > 2 & formData.lastName.length > 2 & formData.email.includes("@") & formData.password===formData.passwordConfirm & formData.passwordConfirm.length>2){
+        if (formData.firstName.length > 2 & formData.lastName.length > 0 & formData.email.includes("@") & formData.password===formData.passwordConfirm & formData.passwordConfirm.length>2){
             setOpenModal(prevModal=>!prevModal)
-            console.log("success")
+            console.log("modal open")
         }
         else {
-            console.log("not complete")
+            setOpenModal(false)
         }
     }
-    console.log(openModaL)
 
     function closeModal(){
         setOpenModal(false)
+
+        setFormData({
+        firstName: "",
+        lastName:"",
+        email: "",
+        password: "",
+        passwordConfirm: "",
+        joinMembership: false
+        })
     }
 
     return (
         <div className="spanAll">
-            <form onSubmit={handleSubmit}>
-                <div className="profileImage">
-                    <img src={face2form} className='memeImg' /> 
+            <div className="circleOverlay">
+                <div className="signUp">
+                    <h1>Sign up today</h1>
+                    <p>Enter Details below to continue!</p>
                 </div>
+            </div>
+            <form onSubmit={handleSubmit}>        
                 <div className="spanAllContainer">
                 <div className="registerName">
                    <h2 className="registerNameh2"> {formData.firstName} {formData.lastName} </h2>
                 </div>
-                        {openModaL && <ModalForm open= {openModaL} close={closeModal} title={formData.firstName}/>} 
-            
+    
+                    {openModaL && <ModalForm open= {openModaL} close={closeModal} title={formData.firstName}/>} 
+                
                 <div className="contentEl">
                     <div className="eachBox"><input type="firstName" placeholder='First Name' className='formInputs' name="firstName" onChange={handleChange} value={formData.firstName}></input>{!openModaL &&<BsCheck className={formData.firstName.length > 2 ? "greenTick" : "blackTick" }/>}</div>
-                    <div className="eachBox"><input type="lastName" placeholder='Last Name' className='formInputs' name="lastName" onChange={handleChange} value={formData.lastName}></input>{!openModaL && <BsCheck className={formData.lastName.length > 2 ? "greenTick" : "blackTick" }/>}</div>
+                    <div className="eachBox"><input type="lastName" placeholder='Last Name' className='formInputs' name="lastName" onChange={handleChange} value={formData.lastName}></input>{!openModaL && <BsCheck className={formData.lastName.length > 0 ? "greenTick" : "blackTick" }/>}</div>
                     <div className="eachBox"><input type="email" placeholder='Email' className='formInputs' name="email" onChange={handleChange} value={formData.email}></input>{!openModaL && <BsCheck className={formData.email.includes("@") ? "greenTick" : "blackTick"}/>}</div>
                     <div className="eachBox"><input type="password" placeholder='Password' className='formInputs' name="password" onChange={handleChange} value={formData.password}></input> {!openModaL && <BsCheck className={formData.password.length > 2 ? "greenTick" : "blackTick" }/>}</div>
                     <div className="eachBox"><input type="password" placeholder='ConfirmPassword' className='formInputs' name="passwordConfirm" onChange={handleChange} value={formData.passwordConfirm}></input>{!openModaL && <BsCheck className={formData.password===formData.passwordConfirm && formData.passwordConfirm.length>2 ? "greenTick" : "blackTick" }/>}</div>
                 </div>
-    {formData.password!==formData.passwordConfirm && formData.passwordConfirm.length>2 && <p className="pwordError">*Passwords Do Not Match</p>} 
-                <div className="genderId">
-                    <label htmlFor="gender" className="gender">Gender</label>
-                    <select className="genderSelect">
-                    {/* onChange= { (e) =>  setFormData(prevFormData => {
-                        return ({...prevFormData})
-                                }
-                            )
-                        } */}
-                    <option value="">--Choose--</option>
-                    <option value="male">Male</option>
-                    <option value="female">Female</option>
-                    </select>
+                <div>
+                    {formData.password!==formData.passwordConfirm && formData.passwordConfirm.length>2 && <p className="pwordError">*Passwords Do Not Match</p>} 
                 </div>
-
-                {/* How to use minlength validator to validate password lenfth etc? */}
-                <div className="form--marketing">
+                <div className="formMarketing">
                     <input
                         id="joinMembership"
                         type="checkbox"
@@ -94,32 +91,14 @@ export function Content(){
                         onChange={handleChange}
                         checked={formData.joinMembership}
                     />
-                    <label htmlFor="joinMembership">I want to sign up to paid Membership</label>
+                    <label htmlFor="joinMembership">I want to receive exclusive content</label>
                 </div>
-                {formData.password===formData.passwordConfirm && formData.passwordConfirm.length>2 && <button className="submitBtn">Submit</button>}
+{}
+                {formData.firstName.length > 2 && formData.lastName.length > 0 && formData.email.includes("@") && formData.password===formData.passwordConfirm && formData.passwordConfirm.length>2 && <button className="submitBtn">Sign Up</button>}
                 </div>
             </form>
         </div>
 
-
-        // <div className="spanAll">
-        //     <div className="box1">
-        //         <h2>hi</h2>
-        //     </div>
-
-        //     <div className="box2">
-        //     <h1>hello baby 2</h1>
-        //     </div>
-
-        //     <div className="box3">
-        //     <h1>hello baby 3</h1>
-        //     </div>
-
-        //     <div className="box4">
-        //     <h1>hello baby 4</h1>
-        //     </div>
-        // </div>
-       
     )
 }
 
--- src/formSite/modalForm.js
+++ src/formSite/modalForm.js
@@ -1,9 +1,12 @@
 import React from "react";
 import {BsPatchCheck} from 'react-icons/bs';
+import gitSheet from '../formSite/git-sheet.pdf'
 import './modalFormCss.css';
 
 
 export default function modalForm(props) {
+
+    const capitalizedName = props.title.charAt(0).toUpperCase() + props.title.slice(1)
 
     if (!props.open){
         return null
@@ -11,11 +14,12 @@ export default function modalForm(props) {
     else{
         return (
             <div className="modalContainer">
-            <div className="closeBtn"><span className="closeBtnSpan" onClick={props.close}>x</span></div>
+            <div className="closeBtn"><span className="closeBtnSpan" onClick={props.close}>✖</span></div>
                 <div className="checkContainer"><BsPatchCheck className="BsPatchCheck" /></div>
             <div className="contentModalContainer">
-                <h2 id="h2Content">Thanks! You're all signed up {props.title}!</h2>
-                <p id="paraContent">Check your Inbox! A confirmation email is on its way...</p>
+                <h2 id="h2Content">Thanks! You're all signed up {capitalizedName}!</h2>
+                <p id="paraContent">Click below to claim your free download!</p>
+                <a href={gitSheet} download="gitSheet" className="downloadBtn">Download</a>
             </div>
             </div>
             );
